93b66d2059a09a0e4c44a25314fc5107eb56d9db
mashihill <keeneking@gmail.com>
2019-08-27 02:39:47 -0400

    finish sql_query

--- sql_queries.py
+++ sql_queries.py
@@ -119,30 +119,95 @@ CREATE TABLE IF NOT EXISTS time
 );
 """)
 
+# staging_events_copy = ("""copy staging_events from {} \
+#                       iam_role {} \
+#                       region 'us-west-2' FORMAT AS JSON {};\
+#                       """).
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+copy staging_events from '{}'  
+credentials 'aws_iam_role={}' 
+region 'us-west-2' FORMAT AS JSON {}; 
+""").format(config.get("S3","LOG_DATA"),
+            config.get("IAM_ROLE","ARN"),
+            config.get("S3","LOG_JSONPATH"))
+
 
 staging_songs_copy = ("""
-""").format()
+copy staging_songs from '{}'  
+credentials 'aws_iam_role={}' 
+region 'us-west-2' FORMAT AS JSON 'auto'; 
+""").format(config.get("S3","LOG_DATA"),
+            config.get("IAM_ROLE","ARN"))
 
 # FINAL TABLES
-
 songplay_table_insert = ("""
+INSERT INTO songplays 
+(
+    start_time, 
+    user_id, 
+    level, 
+    song_id, 
+    artist_id, 
+    session_id, 
+    location, 
+    user_agent
+) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users 
+(
+    user_id, 
+    first_name, 
+    last_name, 
+    gender, 
+    level
+) VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO UPDATE
+    SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
+INSERT INTO songs 
+(
+    song_id, 
+    title, 
+    artist_id, 
+    year, 
+    duration
+) VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) 
+DO NOTHING;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists 
+(
+    artist_id, 
+    name, 
+    location, 
+    latitude, 
+    longitude
+) VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) 
+DO NOTHING;
 """)
 
 time_table_insert = ("""
+INSERT INTO time 
+(
+    start_time, 
+    hour, 
+    day, 
+    week, 
+    month, 
+    year, 
+    weekday
+) VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # QUERY LISTS
