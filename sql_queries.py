f68972aa697270e82af9baaccc5c776c30f1d15b
mashihill <keeneking@gmail.com>
2019-08-27 01:28:56 -0400

    create.py pass

--- sql_queries.py
+++ sql_queries.py
@@ -7,36 +7,118 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP table IF EXISTS staging_events"
+staging_songs_table_drop = "DROP table IF EXISTS staging_songs"
+songplay_table_drop = "DROP table IF EXISTS songplays"
+user_table_drop = "DROP table IF EXISTS users"
+song_table_drop = "DROP table IF EXISTS songs"
+artist_table_drop = "DROP table IF EXISTS artists"
+time_table_drop = "DROP table IF EXISTS time"
+
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events
+(
+    artist varchar,
+    auth varchar,
+    firstName varchar,
+    gender varchar,
+    itemInSession int,
+    lastName varchar,
+    length numeric,
+    level varchar,
+    location varchar,
+    method varchar,
+    page varchar,
+    registration numeric,
+    sessionId int,
+    song varchar,
+    status int,
+    ts bigint,
+    userAgent varchar,
+    userId varchar
+);
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs 
+(
+   song_id varchar PRIMARY KEY,
+   title varchar,
+   duration numeric,
+   year numeric,
+   num_songs numeric,
+   artist_id varchar,
+   artist_name varchar,
+   artist_latitude numeric,
+   artist_longitude numeric,
+   artist_location varchar
+);
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays 
+(
+    songplay_id int IDENTITY(0,1) PRIMARY KEY, 
+    start_time bigint NOT NULL, 
+    user_id int NOT NULL, 
+    level varchar, 
+    song_id varchar, 
+    artist_id varchar, 
+    session_id int, 
+    location varchar, 
+    user_agent varchar
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users 
+(
+    user_id int PRIMARY KEY, 
+    first_name varchar, 
+    last_name varchar, 
+    gender varchar, 
+    level varchar
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs 
+(
+    song_id varchar PRIMARY KEY, 
+    title varchar NOT NULL, 
+    artist_id varchar NOT NULL, 
+    year int, 
+    duration numeric
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists 
+(
+    artist_id varchar PRIMARY KEY, 
+    name varchar NOT NULL, 
+    location varchar, 
+    latitude numeric, 
+    longitude numeric
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time 
+(
+    start_time bigint NOT NULL, 
+    hour int, 
+    day int, 
+    week int, 
+    month int, 
+    year int, 
+    weekday int
+);
 """)
+
 
 # STAGING TABLES
 
